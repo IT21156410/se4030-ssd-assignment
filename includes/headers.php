4ac6facf4a519644e42612e46fa59c4e350aff52
IT21169908 - Prashantha K.G.M <it21169908@my.sliit.lk>
2024-09-16 20:38:56 +0530

    Fix: Content Security Policy (CSP) Header Not Set vulnerability and CSP violation logging

--- includes/headers.php
+++ includes/headers.php
@@ -11,3 +11,34 @@ header('X-Content-Type-Options: nosniff');
  * need to add the X-Frame-Options header to prevent the pages from being embedded in an iframe.
  * */
 header('X-Frame-Options: DENY');
+
+/*
+ * To fix the "Content Security Policy (CSP) Header Not Set" vulnerability,
+ * need to set the Content-Security-Policy (CSP) header.
+ * self -> Allow src from the same domain
+ * data: -> Allow base64-encoded images
+ * External sources (currently not used, but added coz that are commonly used):
+ *  - Allow jQuery from the official CDN for JavaScript functionality
+ *  - Allow styles from Google Fonts for typography
+ *  - Allow font files from Google Fonts' gstatic domain
+ * */
+header("Content-Security-Policy: 
+    default-src 'self'; 
+    script-src 'self' https://code.jquery.com; 
+    style-src 'self' https://fonts.googleapis.com; 
+    font-src 'self' https://fonts.gstatic.com; 
+    img-src 'self' data:; 
+    object-src 'none'; 
+    base-uri 'self'; 
+    frame-ancestors 'none'; 
+    form-action 'self'; 
+    report-uri /app-csp-violation;
+    report-to default;
+");
+
+// Add Report-To header for structured CSP violation reporting
+header("Report-To: { 
+    \"group\": \"default\", 
+    \"max_age\": 10886400, 
+    \"endpoints\": [{ \"url\": \"/app-csp-violation\" }] 
+}");
