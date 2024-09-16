a1cc922926bf00a16d1ce06e43861a5f5d1447ec
IT21169908 - Prashantha K.G.M <it21169908@my.sliit.lk>
2024-09-16 19:47:42 +0530

    Fix: Anti-clickjacking Header vulnerability

--- includes/headers.php
+++ includes/headers.php
@@ -5,3 +5,9 @@
  * X-Content-Type-Options HTTP header need properly set to nosniff.
  * */
 header('X-Content-Type-Options: nosniff');
+
+/*
+ * To fix the "Missing Anti-clickjacking Header" vulnerability,
+ * need to add the X-Frame-Options header to prevent the pages from being embedded in an iframe.
+ * */
+header('X-Frame-Options: DENY');
